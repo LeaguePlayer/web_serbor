2daff5ab3604eef584f3c45cbdd838a9f552d67f
DiggerZ <DiggerZ@mail.ru>
2013-11-11 12:26:59 +0600

    page 404

--- themes/serbor/views/layouts/error_layouts.php
+++ themes/serbor/views/layouts/error_layouts.php
@@ -1,16 +1,5 @@
-<?php $this->beginContent('//layouts/main'); ?>
-<header id="header" class="container">
-    <div class="row">
-        <div class="row">
-            <div class="root-icons">
-                <div class="grid_12">
-                    <?php $this->renderPartial('//page/social_links')?>
-                </div>
-            </div>
-        </div>
-    </div>
-</header>     
-<div id="main" class="container">
+<?php $this->beginContent('//layouts/error_main'); ?>
+    
     <?php echo $content;?>
-</div>
+
 <?php $this->endContent(); ?>
