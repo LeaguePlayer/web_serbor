71404145bedb7d3edf322ef3f73a71c76f65af01
DiggerZ <DiggerZ@mail.ru>
2013-11-26 09:53:43 +0600

    social link

--- themes/serbor/views/page/social_links.php
+++ themes/serbor/views/page/social_links.php
@@ -1,6 +1,6 @@
 <ul>
     <li class="rss"><a href="/news.htm"><img src="/files/images/site/icon_rss.png"></a></li>
     <li class="map"><a href="/map.htm"><img src="/files/images/site/icon_map.png"></a></li>
-    <li class="vk"><a href="http://vk.com/vashazemlya"><img src="/files/images/site/icon_vk.png"></a></li>
+    <li class="vk" target="_blank"><a href="http://vk.com/malkovo72"><img src="/files/images/site/icon_vk.png"></a></li>
     <li class="mail"><a href="/contact.htm"><img src="/files/images/site/icon_mail.png"></a></li>
 </ul>
