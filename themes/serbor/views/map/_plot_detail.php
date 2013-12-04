9f5d3988aa5265d072a4dcebe016e64d450d6b0f
Gallyamov Vitaly <vitgvr@gmail.com>
2013-12-04 14:52:10 +0600

    fix map tultip

--- themes/serbor/views/map/_plot_detail.php
+++ themes/serbor/views/map/_plot_detail.php
@@ -1,10 +1,10 @@
 <div class="tip_plots">
     <div class="plots_details">
         <div class="plot" data-id="<?=$plot->id?>">
-            <p class="plot_number">№ <?=CHtml::encode($plot->num)?></p>
+            <?php if($plot->num){?> <p class="plot_number">№ <?=CHtml::encode($plot->num)?></p> <?}?>
             <p class="plot_status <?=($plot->status == 0 ? '' : 'busy')?>"><?=CHtml::encode(Plots::getStatus($plot->status))?></p>
-            <p class="plot_size">Площадь: <b><?=CHtml::encode($plot->sq)?> кв.м.</b></p>
-            <p class="plot_price">Стоимость: <b><?=number_format(CHtml::encode($plot->price), 0, ' ', ' ')?> рублей</b></p>
+            <?php if($plot->sq){?> <p class="plot_size">Площадь: <b><?=CHtml::encode($plot->sq)?> кв.м.</b></p> <?}?>
+            <?php if($plot->price){?> <p class="plot_price">Стоимость: <b><?=number_format(CHtml::encode($plot->price), 0, ' ', ' ')?> рублей</b></p> <?}?>
             <? if($plot->status == 0):?>
                 <a href="/contact/?pid=<?=$plot->id?>#contact_form" class="reserve_button">Забронировать участок</a>
             <? endif; ?>
